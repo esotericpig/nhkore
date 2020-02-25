9fdbc87db1650a80ef693d26e5f1031d96bc274c
esotericpig <esotericpig@users.noreply.github.com>
2020-02-25 12:10:50 +0800

    Initial commit of NHKNewsWebEasy

---
+++ lib/nhkore/nhk_news_web_easy.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+# frozen_string_literal: true
+
+#--
+# This file is part of NHKore.
+# Copyright (c) 2020 Jonathan Bradley Whited (@esotericpig)
+# 
+# NHKore is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# NHKore is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with NHKore.  If not, see <https://www.gnu.org/licenses/>.
+#++
+
+
+require 'psychgus'
+require 'nhkore/util'
+
+
+module NHKore
+  ###
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  0.2.0
+  ###
+  class NHKNewsWebEasy
+    DEFAULT_FILE = 'nhk_news_web_easy.yml'
+    
+    attr_accessor :articles
+    
+    def initialize()
+      super()
+      
+      @articles = {}
+    end
+    
+    def encode_with(coder)
+      coder[:articles] = @articles
+    end
+    
+    def self.load_data(data,file: nil,**kargs)
+      data = Psych.safe_load(data,
+        aliases: true,
+        filename: file,
+        #freeze: true, # Not in this current version of Psych
+        permitted_classes: [Symbol],
+        symbolize_names: true,
+        **kargs
+      )
+      
+      articles = data[:articles]
+      
+      nhk_news = NHKNewsWebEasy.new()
+      
+      nhk_news.articles = articles unless articles.nil?()
+      
+      return nhk_news
+    end
+    
+    def self.load_file(file=File.join(Util::CORE_DIR,DEFAULT_FILE),mode: 'r:BOM|UTF-8',**kargs)
+      data = File.read(file,mode: mode,**kargs)
+      
+      return load_data(data,file: file,**kargs)
+    end
+    
+    def save_file(file=File.join(Util::CORE_DIR,DEFAULT_FILE),mode: 'wt',**kargs)
+      File.open(file,mode: mode,**kargs) do |file|
+        file.write(to_s())
+      end
+    end
+    
+    def to_s()
+      return Psychgus.dump(self,stylers: [
+        Psychgus::NoSymStyler.new(cap: false),
+        Psychgus::NoTagStyler.new()
+      ])
+    end
+  end
+end
