ca6d384a9cc2c810828cd833700736cbc2ad51d0
esotericpig <esotericpig@users.noreply.github.com>
2020-03-08 20:49:21 +0800

    Add initial scraping code

--- lib/nhkore/search_scraper.rb
+++ lib/nhkore/search_scraper.rb
@@ -22,7 +22,9 @@
 
 
 require 'nhkore/scraper'
+require 'nhkore/search_link'
 require 'nokogiri'
+require 'uri'
 
 
 module NHKore
@@ -31,6 +33,8 @@ module NHKore
   # @since  0.2.0
   ###
   class SearchScraper < Scraper
+    YASASHII_SITE = 'nhk.or.jp/news/easy/'
+    
     def initialize(url,**kargs)
       super(url,**kargs)
     end
@@ -43,6 +47,39 @@ module NHKore
   class BingScraper < SearchScraper
     def initialize(url,**kargs)
       super(url,**kargs)
+    end
+    
+    def self.build_url(site,count: 100)
+      url = ''.dup()
+      
+      url << 'https://www.bing.com/search?'
+      url << URI.encode_www_form(
+        q: "site:#{site}",
+        count: count
+      )
+      
+      return url
+    end
+    
+    def self.build_yasashii_url(**kargs)
+      return build_url(YASASHII_SITE,**kargs)
+    end
+    
+    # TODO: need to do all pages by grabbing page 1,2,3,... from bottom
+    # TODO: add user-agent, etc.
+    def scrape()
+      doc = html_doc()
+      links = SearchLinks.new()
+      
+      # FIXME: check if < 0 && nil/empty
+      # FIXME: choose yasashii/futsuu
+      doc.css('a').each() do |anchor|
+        href = anchor['href'].to_s().downcase()
+        
+        next unless href.include?(YASASHII_SITE)
+        
+        puts href
+      end
     end
   end
 end
