8257d94e359b43d97d8000f45b626b5d0b3a775e
esotericpig <esotericpig@users.noreply.github.com>
2020-03-08 14:50:04 +0800

    Add FutsuuNews, YasashiiNews; add add_article(), sha256s; make News generic

--- lib/nhkore/news.rb
+++ lib/nhkore/news.rb
@@ -22,6 +22,7 @@
 
 
 require 'nhkore/article'
+require 'nhkore/error'
 require 'nhkore/util'
 require 'psychgus'
 
@@ -31,22 +32,41 @@ module NHKore
   # @author Jonathan Bradley Whited (@esotericpig)
   # @since  0.2.0
   ###
-  class NHKNewsWebEasy
-    DEFAULT_FILE = 'nhk_news_web_easy.yml'
+  class News
+    DEFAULT_DIR = Util::CORE_DIR
     
     attr_reader :articles
+    attr_reader :sha256s
     
     def initialize()
       super()
       
       @articles = {}
+      @sha256s = {}
+    end
+    
+    def add_article(key,article)
+      raise ArgError,"duplicate article[#{key}] in articles" if @articles.key?(key)
+      raise ArgError,"duplicate sha256[#{article.sha256}] in articles" if @sha256s.key?(article.sha256)
+      
+      @articles[key] = article
+      @sha256s[article.sha256] = article.url
+      
+      return self
+    end
+    
+    def self.default_file(filename)
+      return File.join(DEFAULT_DIR,filename)
     end
     
     def encode_with(coder)
+      # Order matters.
+      # Don't output @sha256s.
+      
       coder[:articles] = @articles
     end
     
-    def self.load_data(data,file: nil,**kargs)
+    def self.load_data(data,article_class: Article,file: nil,news_class: News,**kargs)
       data = Psych.safe_load(data,
         aliases: true,
         filename: file,
@@ -58,36 +78,79 @@ module NHKore
       
       articles = data[:articles]
       
-      nhk_news_ez = NHKNewsWebEasy.new()
+      news = news_class.new()
       
       if !articles.nil?()
         articles.each() do |key,hash|
           key = key.to_s() # Change from a symbol
-          nhk_news.articles[key] = Article.load_data(key,hash)
+          news.add_article(key,article_class.load_data(key,hash))
         end
       end
       
-      return nhk_news_ez
+      return news
     end
     
-    def self.load_file(file=File.join(Util::CORE_DIR,DEFAULT_FILE),mode: 'r:BOM|UTF-8',**kargs)
+    def self.load_file(file,mode: 'r:BOM|UTF-8',**kargs)
       data = File.read(file,mode: mode,**kargs)
       
       return load_data(data,file: file,**kargs)
     end
     
-    def save_file(file=File.join(Util::CORE_DIR,DEFAULT_FILE),mode: 'wt',**kargs)
+    def save_file(file,mode: 'wt',**kargs)
       File.open(file,mode: mode,**kargs) do |file|
         file.write(to_s())
       end
     end
     
     def to_s()
-      return Psychgus.dump(self,stylers: [
-        Psychgus::FlowStyler.new(8), # Put each Word on one line (flow/inline style)
-        Psychgus::NoSymStyler.new(cap: false), # Remove symbols, don't capitalize
-        Psychgus::NoTagStyler.new() # Remove class names (tags)
-      ])
+      return Psychgus.dump(self,
+        line_width: 10000, # Try not to wrap; ichiman!
+        stylers: [
+          Psychgus::FlowStyler.new(8), # Put each Word on one line (flow/inline style)
+          Psychgus::NoSymStyler.new(cap: false), # Remove symbols, don't capitalize
+          Psychgus::NoTagStyler.new() # Remove class names (tags)
+        ]
+      )
+    end
+  end
+  
+  ###
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  0.2.0
+  ###
+  class FutsuuNews < News
+    DEFAULT_FILE = default_file('nhk_news_web_regular.yml')
+    
+    def self.load_data(data,**kargs)
+      return News.load_data(data,article_class: Article,news_class: FutsuuNews,**kargs)
+    end
+    
+    def self.load_file(file=DEFAULT_FILE,**kargs)
+      return News.load_file(file,article_class: Article,news_class: FutsuuNews,**kargs)
+    end
+    
+    def save_file(file=DEFAULT_FILE,**kargs)
+      super(file,**kargs)
+    end
+  end
+  
+  ###
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  0.2.0
+  ###
+  class YasashiiNews < News
+    DEFAULT_FILE = default_file('nhk_news_web_easy.yml')
+    
+    def self.load_data(data,**kargs)
+      return News.load_data(data,article_class: Article,news_class: YasashiiNews,**kargs)
+    end
+    
+    def self.load_file(file=DEFAULT_FILE,**kargs)
+      return News.load_file(file,article_class: Article,news_class: YasashiiNews,**kargs)
+    end
+    
+    def save_file(file=DEFAULT_FILE,**kargs)
+      super(file,**kargs)
     end
   end
 end
