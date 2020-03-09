4fbc17a8f536bf4dd2dad1d5ca85169c0640f2e6
esotericpig <esotericpig@users.noreply.github.com>
2020-03-09 13:20:37 +0800

    Add header for HTTP header fields & User-Agent

--- lib/nhkore/scraper.rb
+++ lib/nhkore/scraper.rb
@@ -31,23 +31,39 @@ module NHKore
   # @since  0.2.0
   ###
   class Scraper
+    # Copied from googler (https://github.com/jarun/googler).
+    USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    
     attr_accessor :str_or_io
     attr_accessor :url
     
-    def initialize(url,file: false,str_or_io: nil,**kargs)
+    # Pass in +header: {}+ to set the default HTTP header fields.
+    def initialize(url,file: false,header: nil,str_or_io: nil,**kargs)
       super()
+      
+      if !header.nil?() && !file
+        # Some sites (Search Engines) hate scrapers, so need HTTP header fields.
+        # If this isn't enough, look at googler for more header fields to set:
+        # - https://github.com/jarun/googler
+        # If necessary, can use the Faraday, HTTParty, or RestClient gem and
+        #   pass in to str_or_io.
+        
+        header['User-Agent'] = USER_AGENT unless header.key?('User-Agent')
+        
+        kargs.merge!(header) # header will overwrite duplicate kargs entries
+      end
       
       @url = url
       
       if str_or_io.nil?()
         if file
           # NHK's website tends to always use UTF-8
-          @str_or_io = File.open(url,'rt:UTF-8')
+          @str_or_io = File.open(url,'rt:UTF-8',**kargs)
         else
           # Use URI.open() instead of (Kernel.)open() for safety (code-injection attack).
           # Disable redirect for safety (infinite-loop attack).
           # - All options: https://ruby-doc.org/stdlib-2.7.0/libdoc/open-uri/rdoc/OpenURI/OpenRead.html
-          @str_or_io = URI.open(url,redirect: false)
+          @str_or_io = URI.open(url,redirect: false,**kargs)
         end
       else
         @str_or_io = str_or_io
