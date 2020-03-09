00403b5f36f15e0a1f43be2595e9e624cb123b35
esotericpig <esotericpig@users.noreply.github.com>
2020-03-09 13:27:58 +0800

    Add site & regex; use HTTP header fields

--- lib/nhkore/search_scraper.rb
+++ lib/nhkore/search_scraper.rb
@@ -21,8 +21,10 @@
 #++
 
 
+require 'nhkore/error'
 require 'nhkore/scraper'
 require 'nhkore/search_link'
+require 'nhkore/util'
 require 'nokogiri'
 require 'uri'
 
@@ -33,10 +35,17 @@ module NHKore
   # @since  0.2.0
   ###
   class SearchScraper < Scraper
+    FUTSUU_SITE = 'nhk.or.jp/news/html/'
     YASASHII_SITE = 'nhk.or.jp/news/easy/'
     
-    def initialize(url,**kargs)
-      super(url,**kargs)
+    # https://www3.nhk.or.jp/news/html/20200220/k10012294001000.html
+    FUTSUU_REGEX = /#{Regexp.quote(FUTSUU_SITE)}.+\/k.+\.html?/i
+    # https://www3.nhk.or.jp/news/easy/k10012294001000/k10012294001000.html
+    YASASHII_REGEX = /#{Regexp.quote(YASASHII_SITE)}k.+\/k.+\.html?/i
+    
+    # Pass in +header: {}+ to trigger using the default HTTP header fields.
+    def initialize(url,header: {},**kargs)
+      super(url,header: header,**kargs)
     end
   end
   
@@ -45,11 +54,33 @@ module NHKore
   # @since  0.2.0
   ###
   class BingScraper < SearchScraper
-    def initialize(url,**kargs)
+    attr_reader :regex
+    attr_reader :site
+    
+    def initialize(site,regex: nil,url: nil,**kargs)
+      case site
+      when :futsuu
+        regex = FUTSUU_REGEX if regex.nil?()
+        site = FUTSUU_SITE
+      when :yasashii
+        regex = YASASHII_REGEX if regex.nil?()
+        site = YASASHII_SITE
+      else
+        site = Util.strip_web_str(site.to_s())
+        regex = /#{Regexp.quote(site)}/ if regex.nil?()
+      end
+      
+      raise ArgError,"empty regex[#{regex}]" if regex.nil?()
+      raise ArgError,"empty site[#{site}]" if site.empty?()
+      
+      @regex = regex
+      @site = site
+      url = self.class.build_url(site,**kargs) if url.nil?()
+      
       super(url,**kargs)
     end
     
-    def self.build_url(site,count: 100)
+    def self.build_url(site,count: 100,**kargs)
       url = ''.dup()
       
       url << 'https://www.bing.com/search?'
@@ -61,25 +92,38 @@ module NHKore
       return url
     end
     
-    def self.build_yasashii_url(**kargs)
-      return build_url(YASASHII_SITE,**kargs)
-    end
-    
-    # TODO: need to do all pages by grabbing page 1,2,3,... from bottom
-    # TODO: add user-agent, etc.
-    def scrape()
+    # TODO: probably don't call it 'first' but 'from_count' or something
+    def scrape(links,first)
       doc = html_doc()
-      links = SearchLinks.new()
       
-      # FIXME: check if < 0 && nil/empty
-      # FIXME: choose yasashii/futsuu
+      next_first = -1
+      next_page = nil
+      
+      # FIXME: check if < 0 && nil/empty, etc.
       doc.css('a').each() do |anchor|
-        href = anchor['href'].to_s().downcase()
+        href = anchor['href'].to_s()
+        href = Util.unspace_web_str(href)
         
-        next unless href.include?(YASASHII_SITE)
+        next if href.empty?()
+        
+        href = href.downcase()
+        
+        if (md = href.match(/first\=(\d+)/))
+          i = md[1].to_i()
+          
+          if i > first && (next_first == -1 || i < next_first)
+            next_first = i
+            next_page = href
+          end
+        end
+        
+        next unless href =~ regex
         
         puts href
       end
+      
+      # TODO: make this a class?
+      return [next_page,next_first]
     end
   end
 end
