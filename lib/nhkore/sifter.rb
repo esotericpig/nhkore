5d25c4cb6e35003bb9b7bcdcc3b55cfb9d7ae69d
esotericpig <esotericpig@users.noreply.github.com>
2020-03-21 20:30:16 +0800

    Add caption, ignore!(), put_html!(), put_yaml!()

--- lib/nhkore/sifter.rb
+++ lib/nhkore/sifter.rb
@@ -22,6 +22,7 @@
 
 
 require 'csv'
+require 'psychgus'
 
 require 'nhkore/util'
 
@@ -34,8 +35,8 @@ module NHKore
   class Sifter
     DEFAULT_DIR = Util::CORE_DIR
     
-    DEFAULT_FUTSUU_FILENAME = 'nhk_news_web_regular.csv'
-    DEFAULT_YASASHII_FILENAME = 'nhk_news_web_easy.csv'
+    DEFAULT_FUTSUU_FILENAME = 'sift_nhk_news_web_regular'
+    DEFAULT_YASASHII_FILENAME = 'sift_nhk_news_web_easy'
     
     def self.build_file(filename)
       return File.join(DEFAULT_DIR,filename)
@@ -45,9 +46,15 @@ module NHKore
     DEFAULT_YASASHII_FILE = build_file(DEFAULT_YASASHII_FILENAME)
     
     attr_accessor :articles
+    attr_accessor :caption
+    attr_accessor :ignores
+    attr_accessor :output
     
     def initialize(news)
       @articles = news.articles.values.dup()
+      @caption = nil
+      @ignores = {}
+      @output = nil
     end
     
     def filter_by_datetime!(datetime_filter=nil,from_filter: nil,to_filter: nil)
@@ -108,9 +115,135 @@ module NHKore
       return self
     end
     
+    def ignore!(key)
+      @ignores[key] = true
+      
+      return self
+    end
+    
+    # This does not output {caption}.
+    def put_csv!()
+      words = sift()
+      
+      @output = CSV.generate(headers: :first_row,write_headers: true) do |csv|
+        row = ['Frequency','Word','Kana','English']
+        row << 'Definition' unless @ignores[:defn]
+        csv << row
+        
+        words.each() do |word|
+          row = [word.freq,word.word,word.kana,word.eng]
+          row << word.defn unless @ignores[:defn]
+          csv << row
+        end
+      end
+      
+      return @output
+    end
+    
+    def put_html!()
+      words = sift()
+      
+      @output = ''.dup()
+      
+      @output << <<~EOH
+        <!DOCTYPE html>
+        <html lang="ja">
+        <head>
+          <meta charset="utf-8">
+          <title>NHKore</title>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Serif+JP">
+          <style>
+            body {
+              background-color: #FCFBF9;
+              color: #333333;
+              font-family: 'Noto Serif JP',Verdana,sans-serif;
+            }
+            h1 {
+              color: #737373;
+            }
+            table {
+              border-collapse: collapse;
+              width: 100%;
+            }
+            td,th {
+              border: 1px solid #333333;
+              padding: 8px;
+              text-align: left;
+            }
+            th {
+              background-color: #082A8E;
+              color: #FCFBF9;
+            }
+            tr:nth-child(even) {
+              background-color: #A5C7ED;
+            }
+            tr:hover {
+              background-color: #FFDDCA;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>NHKore</h1>
+          <h2>#{@caption}</h2>
+          
+          <table>
+      EOH
+      #" # Fix for editor
+      
+      @output << '    <tr><th>Frequency</th><th>Word</th><th>Kana</th><th>English</th>'
+      @output << '<th>Definition</th>' unless @ignores[:defn]
+      @output << "</tr>\n"
+      
+      words.each() do |word|
+        @output << '    <tr>'
+        @output << "<td>#{Util.escape_html(word.freq.to_s())}</td>"
+        @output << "<td>#{Util.escape_html(word.word.to_s())}</td>"
+        @output << "<td>#{Util.escape_html(word.kana.to_s())}</td>"
+        @output << "<td>#{Util.escape_html(word.eng.to_s())}</td>"
+        @output << "<td>#{Util.escape_html(word.defn.to_s())}</td>" unless @ignores[:defn]
+        @output << "</tr>\n"
+      end
+      
+      @output << <<~EOH
+          </table>
+        </body>
+        </html>
+      EOH
+      #/ # Fix for editor
+      
+      return @output
+    end
+    
+    def put_yaml!()
+      words = sift()
+      
+      # Just blank out ignores.
+      if !@ignores.empty?()
+        words.each() do |word|
+          word.defn = nil if @ignores[:defn]
+        end
+      end
+      
+      yaml = {
+        caption: @caption,
+        words: words
+      }
+      
+      @output = Psychgus.dump(yaml,
+        line_width: 10000, # Try not to wrap; ichiman!
+        stylers: [
+          Psychgus::FlowStyler.new(4), # Put each Word on one line (flow/inline style)
+          Psychgus::NoSymStyler.new(cap: false), # Remove symbols, don't capitalize
+          Psychgus::NoTagStyler.new() # Remove class names (tags)
+        ]
+      )
+      
+      return @output
+    end
+    
     def save_file(file,mode: 'wt',**kargs)
       File.open(file,mode: mode,**kargs) do |fout|
-        fout.write(to_s(**kargs))
+        fout.write(to_s())
       end
     end
     
@@ -137,22 +270,8 @@ module NHKore
       return words
     end
     
-    def to_s(defn: true,**kargs)
-      words = sift()
-      
-      csv = CSV.generate(headers: :first_row,write_headers: true) do |csv|
-        row = ['Frequency','Word','Kana','English']
-        row << 'Definition' if defn
-        csv << row
-        
-        words.each() do |word|
-          row = [word.freq,word.word,word.kana,word.eng]
-          row << word.defn if defn
-          csv << row
-        end
-      end
-      
-      return csv
+    def to_s()
+      return @output.to_s()
     end
   end
 end
